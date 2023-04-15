e920bc0472892e2221a4e432c4f964e341a66f35
TheIrishBearcat24 <61879913+TheIrishBearcat24@users.noreply.github.com>
2023-04-15 14:55:24 -0400

    Progress

--- SmokersProblem.cpp
+++ SmokersProblem.cpp
@@ -1,123 +1,22 @@
-#include <pthread.h>
+#include <chrono>
+#include <thread>
+#include <random>
+#include <iostream>
 #include <semaphore.h>
-#include <string>
-#include <errno.h>
+#include <pthread.h>
 #include <time.h>
-#include "clock.h"
 
 using namespace std;
 
-void paperTobaccoFunction();
-void tobaccoMatchFunction();
-void paperMatchFunction();
-void pushTobacco();
-void pushPaper();
-void pushMatch();
-void smokerWithTobacco();
-void smokerWithPaper();
-void smokerWithMatch();
-
-// agent semaphores
-
-sem_t tobaccoAgentSemaphore;
-sem_t paperAgentSemaphore;
-sem_t matchAgentSemaphore;
-sem_t agentSemaphore;
-
-// flags
-
-bool isTobacco = false;
-bool isPaper = false;
-bool isMatch = false;
-
-// pusher semaphores
-
-sem_t tobaccoPusherSemaphore;
-sem_t paperPusherSemaphore;
-sem_t matchPusherSemaphore;
-
-pthread_mutex_t pMutex;
-
-void * paperTobaccoFunction(void * args) {
-    while (true) {
-        sem_wait(&agentSemaphore);
-        sem_post(&paperAgentSemaphore);
-        sem_post(&tobaccoAgentSemaphore);
-    }
-}
-
-void * tobaccoMatchFunction(void * args) {
-    while (true) {
-        sem_wait(&agentSemaphore);
-        sem_post(&tobaccoAgentSemaphore);
-        sem_post(&matchAgentSemaphore);
-    }
-}
-
-void * paperMatchFunction(void * args) {
-    while (true) {
-        sem_wait(&agentSemaphore);
-        sem_post(&paperAgentSemaphore);
-        sem_post(&matchAgentSemaphore);
-    }
-}
-
-void * pushTobacco(void * args) {
-    while (true) {
-        sem_wait(&tobaccoAgentSemaphore);
-        pthread_mutex_lock(&pMutex);
-
-        if (isPaper) {
-            isPaper = false;
-            sem_post(&matchPusherSemaphore);
-        } else if (isMatch) {
-            isMatch = false;
-            sem_post(&paperPusherSemaphore);
-        } else {
-            isTobacco = true;
-        }
-
-        pthread_mutex_unlock(&pMutex);
-    }
-}
-
-void * pushMatch(void * args) {
-    sem_wait(&matchAgentSemaphore);
-    pthread_mutex_lock(&pMutex);
-
-    if (isPaper) {
-        isPaper = false;
-        sem_post(&tobaccoPusherSemaphore);
-    } else if (isTobacco) {
-        isTobacco = false;
-        sem_post(&paperPusherSemaphore);
-    } else {
-        isMatch = true;
-    }
-
-    pthread_mutex_unlock(&pMutex);
-}
-
-void * pushPaper(void * args) {
-    while (true) {
-        sem_wait(&paperAgentSemaphore);
-        pthread_mutex_lock(&pMutex);
-
-        if (isMatch) {
-            isMatch = false;
-            sem_post(&tobaccoPusherSemaphore);
-        } else if (isTobacco) {
-            isTobacco = false;
-            sem_post(&matchPusherSemaphore);
-        } else {
-            isPaper = true;
-        }
-
-        pthread_mutex_unlock(&pMutex);
-    }
-}
+sem_t match;
+sem_t paper = 0;
+sem_t tobacco = 0;
+sem_t agent = 0;
+sem_t lock;
 
 int main() {
-    cout << "HI I LIKE CARRIE";
+
+    cout << "Hi I really would like to date Carrie Dietz";
+
     return 0;
 }
