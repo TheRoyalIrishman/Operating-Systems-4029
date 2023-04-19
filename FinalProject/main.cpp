d4c697aed702f05312666393a71508c7418dbc15
William Baker <bbrk24@gmail.com>
2023-04-19 09:27:52 -0400

    Add page table

--- FinalProject/main.cpp
+++ FinalProject/main.cpp
@@ -1,16 +1,31 @@
 #include <fstream>
 #include <cstdint>
 #include <iostream>
+#include <map>
 
 using namespace std;
 
 int main() {
     ifstream inputAddressFile("addresses.txt");
+    map<uint8_t, int8_t[]> pageTable;
 
     uint32_t addr;
     while (inputAddressFile >> addr) {
-        addr = addr & 0x0000ffff;
-        cout << addr << endl;
+        uint16_t maskedAddr = addr & 0x0000ffff;
+
+        uint8_t pageNumber = maskedAddr >> 8;
+        uint8_t pageOffset = maskedAddr & 0x000000ff;
+
+        auto pageLocation = pageTable.find(pageNumber);
+        if (pageLocation == pageTable.end()) {
+            // TODO: Load in page from BACKING_STORE.bin file
+        } else {
+            // already loaded in
+            auto page = pageLocation->second;
+            int8_t value = page[pageOffset];
+
+            cout << addr << ' ' << maskedAddr << ' ' << (int)value << endl;
+        }
     }
 
     return 0;
